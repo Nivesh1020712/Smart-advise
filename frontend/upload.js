ceb0f837b5627e401994308fa82ecd11406536fb
Nivesh1020712 <niveshrnk@gmail.com>
2025-07-02 18:56:19 +0100

    Update upload.js

--- frontend/upload.js
+++ frontend/upload.js
@@ -1,1 +1,14 @@
+document.getElementById("uploadForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const file = e.target.elements.file.files[0];
+  const formData = new FormData();
+  formData.append("file", file);
+
+  fetch("http://<your-server>:5000/upload", {
+    method: "POST",
+    body: formData
+  }).then(response => response.json()).then(data => {
+    alert("Prediction: " + data.prediction);
+  });
+});
 
